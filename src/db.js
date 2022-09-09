bc0a73d46bc9bcaa8526c06128370dfe930db7c9
eunseolYun <skopdt73@gmail.com>
2022-09-09 18:35:26 +0900

    mongoDB, mongoose models

---
+++ src/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+//wetube is my db's name ->
+mongoose.connect("mongodb://127.0.0.1:27017/wetube", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅ Connected to DB!");
+const handleError = () => console.log("❌ DB Error", error);
+
+db.on("error", handleError); // error could be many times(like onclick event) so use on()
+db.once("open", handleOpen); //once() is only once happen
