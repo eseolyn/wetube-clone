7078d490c09b0f0f896c361495c05afb88b7a3f1
eunseolYun <skopdt73@gmail.com>
2022-09-25 20:26:25 +0900

    CSS not complete

--- src/controller/videoController.js
+++ src/controller/videoController.js
@@ -22,7 +22,9 @@ export const home = async (req, res) => {
 */
 
 export const home = async (req, res) => {
-  const videos = await Video.find({}).sort({ createdAt: "desc" });
+  const videos = await Video.find({})
+    .sort({ createdAt: "desc" })
+    .populate("owner");
   return res.render("home", { pageTitle: "Home", videos });
 };
 export const watch = async (req, res) => {
